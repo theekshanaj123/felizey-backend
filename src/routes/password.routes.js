3f93a8dee665e30e56d26c06014819dd60045b5d
Kavishka Chathumal <137688716+Kavishka119@users.noreply.github.com>
2025-04-12 11:03:33 +0530

    fix issue

--- src/routes/password.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authenticateToken = require("../middleware/authenticateToken");
-const { sendPasswordResetEmail, passwordReset } = require('../controllers/password.controller');
-
-router.get('/resetMail',authenticateToken , sendPasswordResetEmail);
-router.post('/reset', authenticateToken , passwordReset);
-
-module.exports = router;
--- src/services/supabaseClient.js
+++
@@ -1,9 +0,0 @@
-// services/supabaseClient.js
-const { createClient } = require("@supabase/supabase-js");
-
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
-);
-
-module.exports = supabase;
