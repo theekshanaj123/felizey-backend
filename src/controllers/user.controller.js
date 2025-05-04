104e04a35a3ffe4d949482bf705aa895ca3dcfee
Kavishka Chathumal <137688716+Kavishka119@users.noreply.github.com>
2025-05-04 12:41:40 +0530

    update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,183 +2,205 @@ const prisma = require("../config/db");
 const sendEmail = require("../services/emailSender");
 
 exports.updateUserEmailSend = async (req, res) => {
-  try {
-    const user = await prisma.user.findUnique({
-      where: { email: req.user.email },
-    });
+    try {
+        const user = await prisma.user.findUnique({
+            where: {email: req.user.email},
+        });
 
-    if (!user) {
-      return res.status(400).json({ message: "User Not Found." });
+        if (!user) {
+            return res.status(400).json({message: "User Not Found."});
+        }
+
+        const sendMail = await sendEmail(
+            req.user.email,
+            "Change of Email",
+            req.user.email
+        );
+
+        if (sendMail.error) {
+            return res.status(400).json({message: sendMail.message});
+        }
+
+        // const { error } = await supabase.auth.updateUser({
+        //   email: "kavishkachathumal276@gmail.com",
+        // });
+
+        // if (error) {
+        //   console.error("Email change error:", error.message);
+        //   return res.status(400).json({ message: error.message });
+        // } else {
+        //   console.log("Confirmation email sent to the new address.");
+        //   return res.status(200).json({
+        //     status: true,
+        //     message: `Email update request sent to ${req.user.email}. Verify using the confirmation email.`,
+        //   });
+        // }
+
+        return res.status(200).json({
+            status: true,
+            message: `Email update request sent to ${req.user.email}. Verify using the confirmation email.`,
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(400).json({message: error.message});
     }
-
-    const sendMail = await sendEmail(
-      req.user.email,
-      "Change of Email",
-      req.user.email
-    );
-
-    if (sendMail.error) {
-      return res.status(400).json({ message: sendMail.message });
-    }
-
-    // const { error } = await supabase.auth.updateUser({
-    //   email: "kavishkachathumal276@gmail.com",
-    // });
-
-    // if (error) {
-    //   console.error("Email change error:", error.message);
-    //   return res.status(400).json({ message: error.message });
-    // } else {
-    //   console.log("Confirmation email sent to the new address.");
-    //   return res.status(200).json({
-    //     status: true,
-    //     message: `Email update request sent to ${req.user.email}. Verify using the confirmation email.`,
-    //   });
-    // }
-
-    return res.status(200).json({
-      status: true,
-      message: `Email update request sent to ${req.user.email}. Verify using the confirmation email.`,
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(400).json({ message: error.message });
-  }
 };
 
 exports.updateEmail = async (req, res) => {
-  try {
-    const { newEmail } = req.body;
+    try {
+        const {newEmail} = req.body;
 
-    const user = await prisma.user.findUnique({
-      where: { email: req.user.email },
-    });
+        const user = await prisma.user.findUnique({
+            where: {email: req.user.email},
+        });
 
-    if (!user) {
-      return res.status(400).json({ message: "User Not Found." });
+        if (!user) {
+            return res.status(400).json({message: "User Not Found."});
+        }
+
+        if (!newEmail) {
+            return res.status(400).json({message: "New Email Required."});
+        }
+
+        const updatedUser = await prisma.user.update({
+            where: {
+                email: req.user.email,
+            },
+            data: {
+                email: newEmail,
+            },
+        });
+
+        return res.status(200).json({
+            status: true,
+            message: "Email Change Successfull",
+            data: updatedUser,
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(400).json({message: error.message});
     }
-
-    if (!newEmail) {
-      return res.status(400).json({ message: "New Email Required." });
-    }
-
-    const updatedUser = await prisma.user.update({
-      where: {
-        email: req.user.email,
-      },
-      data: {
-        email: newEmail,
-      },
-    });
-
-    return res.status(200).json({
-      status: true,
-      message: "Email Change Successfull",
-      data: updatedUser,
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(400).json({ message: error.message });
-  }
 };
 
 exports.getUser = async (req, res) => {
-  try {
-    const user = await prisma.user.findUnique({ where: { id: req.query.id } });
+    try {
+        const user = await prisma.user.findUnique({where: {id: req.query.id}});
 
-    if (!user) {
-      return res.status(400).json({ message: "User Not Found." });
+        if (!user) {
+            return res.status(400).json({message: "User Not Found."});
+        }
+
+        return res.status(200).json({
+            status: true,
+            data: user,
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(400).json({message: error.message});
     }
-
-    return res.status(200).json({
-      status: true,
-      data: user,
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(400).json({ message: error.message });
-  }
 };
 
 exports.updateUser = async (req, res) => {
-  try {
-    const {
-      id,
-      firstName,
-      lastName,
-      role,
-      avatar_url,
-      email,
-      phoneNumber,
-      bio,
-      country,
-      instagram,
-      tiktok,
-    } = req.body;
+    try {
+        const {
+            id,
+            firstName,
+            lastName,
+            role,
+            avatar_url,
+            email,
+            phoneNumber,
+            bio,
+            country,
+            instagram,
+            tiktok,
+        } = req.body;
 
-    const user = await prisma.user.findUnique({ where: { id: id } });
+        const user = await prisma.user.findUnique({where: {id: id}});
 
-    if (!user) {
-      return res.status(400).json({ message: "User Not Found." });
-    }
+        if (!user) {
+            return res.status(400).json({message: "User Not Found."});
+        }
 
-    if (!firstName) {
-      return res.status(400).json({ message: "First Name is Required." });
-    }
-    if (!lastName) {
-      return res.status(400).json({ message: "Last Name is Required." });
-    }
-    if (!role) {
-      return res.status(400).json({ message: "Role is Required." });
-    }
-    if (!avatar_url) {
-      return res.status(400).json({ message: "Avatar URL is Required." });
-    }
-    if (!email) {
-      return res.status(400).json({ message: "Email is Required." });
-    }
-    if (!phoneNumber) {
-      return res.status(400).json({ message: "Phone Number is Required." });
-    }
-    if (!bio) {
-      return res.status(400).json({ message: "Bio is Required." });
-    }
-    if (!country) {
-      return res.status(400).json({ message: "Country is Required." });
-    }
-    if (!instagram) {
-      return res
-        .status(400)
-        .json({ message: "Instagram Username is Required." });
-    }
-    if (!tiktok) {
-      return res.status(400).json({ message: "Tiktok Username is Required." });
-    }
+        if (!firstName) {
+            return res.status(400).json({message: "First Name is Required."});
+        }
+        if (!lastName) {
+            return res.status(400).json({message: "Last Name is Required."});
+        }
+        if (!role) {
+            return res.status(400).json({message: "Role is Required."});
+        }
+        if (!avatar_url) {
+            return res.status(400).json({message: "Avatar URL is Required."});
+        }
+        if (!email) {
+            return res.status(400).json({message: "Email is Required."});
+        }
+        if (!phoneNumber) {
+            return res.status(400).json({message: "Phone Number is Required."});
+        }
+        if (!bio) {
+            return res.status(400).json({message: "Bio is Required."});
+        }
+        if (!country) {
+            return res.status(400).json({message: "Country is Required."});
+        }
+        if (!instagram) {
+            return res
+                .status(400)
+                .json({message: "Instagram Username is Required."});
+        }
+        if (!tiktok) {
+            return res.status(400).json({message: "Tiktok Username is Required."});
+        }
 
-    const updateUser = await prisma.user.update({
-      where: {
-        id: id,
-      },
-      data: {
-        firstName: firstName,
-        lastName: lastName,
-        role: role,
-        avatar_url: avatar_url,
-        email: email,
-        phoneNumber: phoneNumber,
-        bio: bio,
-        country: country,
-        instagram: instagram,
-        tiktok: tiktok,
-      },
-    });
+        const updateUser = await prisma.user.update({
+            where: {
+                id: id,
+            },
+            data: {
+                firstName: firstName,
+                lastName: lastName,
+                role: role,
+                avatar_url: avatar_url,
+                email: email,
+                phoneNumber: phoneNumber,
+                bio: bio,
+                country: country,
+                instagram: instagram,
+                tiktok: tiktok,
+            },
+        });
 
-    return res.status(200).json({
-      status: true,
-      data: updateUser,
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(400).json({ message: error.message });
-  }
+        return res.status(200).json({
+            status: true,
+            data: updateUser,
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(400).json({message: error.message});
+    }
 };
+
+exports.getUserByToken = async (req, res) => {
+    try {
+
+        const email = req.user.email;
+
+        const user = await prisma.user.findUnique({where: {email: email}});
+
+        if(!user){
+            return res.status(400).json({message: "User Not Found."});
+        }
+
+        res.status(200).json({
+            status : true,
+            user : user,
+        });
+
+    } catch (e) {
+        console.error(error);
+        return res.status(400).json({message: error.message});
+    }
+}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const authenticateToken = require("../middleware/authenticateToken");
-const { updateUserEmailSend, updateEmail, getUser, updateUser } = require("../controllers/user.controller");
+const { updateUserEmailSend, updateEmail, getUser, updateUser, getUserByToken} = require("../controllers/user.controller");
 
 router.get("/updateEmailSend", authenticateToken, updateUserEmailSend);
 router.post("/updateEmail", authenticateToken, updateEmail);
 router.get("/getUserById", authenticateToken, getUser);
+router.get("/getUser", authenticateToken, getUserByToken);
 router.post("/updateUserById", authenticateToken, updateUser);
 
 module.exports = router;
