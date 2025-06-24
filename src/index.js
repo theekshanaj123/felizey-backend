29dffd6426cfee37c1d29044af183d8f34c51d2a
Kavishka Chathumal <137688716+Kavishka119@users.noreply.github.com>
2025-06-24 16:00:47 +0530

    update

--- src/index.js
+++ src/index.js
@@ -13,6 +13,13 @@ require("dotenv").config();
 
 app.set('trust proxy', true);
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
+  next();
+});
+
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/password", passwordRoutes);
