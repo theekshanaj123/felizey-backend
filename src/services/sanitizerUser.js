37786799f58838eaeb770576a3c00c667f27af8f
Kavishka Chathumal <137688716+Kavishka119@users.noreply.github.com>
2025-09-07 13:45:23 +0530

    update user

--- src/services/sanitizerUser.js
+++ src/services/sanitizerUser.js
@@ -1,22 +1,25 @@
 function sanitizeUser(user) {
-    if (!user) return null;
+  if (!user) return null;
 
-    return {
-        id: user.id,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        role: user.role,
-        created_at: user.created_at,
-        avatar_url: user.avatar_url,
-        email: user.email,
-        verified: user.verified,
-        phoneNumber: user.phoneNumber,
-        bio: user.bio,
-        isPrimium: user.isPrimium,
-        country: user.country,
-        instagram: user.instagram,
-        tiktok: user.tiktok,
-    };
+  return {
+    id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    role: user.role,
+    created_at: user.created_at,
+    avatar_url: user.avatar_url,
+    email: user.email,
+    verified: user.verified,
+    phoneNumber: user.phoneNumber,
+    bio: user.bio,
+    isPrimium: user.isPrimium,
+    country: user.country,
+    instagram: user.instagram,
+    tiktok: user.tiktok,
+    vCode: user.vCode,
+    status: user.status,
+    firebaseToken: user.firebaseToken,
+  };
 }
 
 module.exports = sanitizeUser;
