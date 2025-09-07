3aa5d70af2962d7bad6afde09693db796ae4feaa
Kavishka Chathumal <137688716+Kavishka119@users.noreply.github.com>
2025-09-07 13:51:27 +0530

    update

--- src/routes/notification.routes.js
+++ src/routes/notification.routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const authenticateToken = require("../middleware/authenticateToken");
 const { pushNotification } = require("../controllers/notification.controller");
 
-router.post("/pushNotification/", authenticateToken, pushNotification);
+router.post("/pushNotification", authenticateToken, pushNotification);
 
 module.exports = router;
