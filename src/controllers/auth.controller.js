0dfd6786f2bcce166add9a36c81dcfadae56b886
Kavishka Chathumal <137688716+Kavishka119@users.noreply.github.com>
2025-09-20 14:42:41 +0530

    update

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -267,7 +267,7 @@ a[x-apple-data-detectors],
 
       return res.status(200).json({
         message: "User created",
-        user: { id: user.id, name: user.name, email: user.email, token: token },
+        user: { id: user.id, name: user.name, email: user.email },
       });
     } else {
       return res.status(500).json({ message: error.message });
