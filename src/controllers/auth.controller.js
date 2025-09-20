1e12b58ab7825b1bc02ba8c6c0f3662e3e84bd9b
Kavishka Chathumal <137688716+Kavishka119@users.noreply.github.com>
2025-09-20 13:59:46 +0530

    update

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -38,7 +38,8 @@ exports.register = async (req, res) => {
       },
     });
 
-    const html = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    if (temp_code) {
+      const html = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html dir="ltr" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" lang="en">
  <head>
   <meta charset="UTF-8">
@@ -250,27 +251,30 @@ a[x-apple-data-detectors],
  </body>
 </html>`;
 
-    const sendMail = await sendEmail(email, "Email Verification", html);
+      const sendMail = await sendEmail(email, "Email Verification", html);
 
-    if (sendMail.error) {
-      return res.status(400).json({ message: sendMail.message });
+      if (sendMail.error) {
+        return res.status(400).json({ message: sendMail.message });
+      }
+
+      const token = jwt.sign(
+        {
+          id: user.id,
+          email: user.email,
+        },
+        process.env.JWT_SECRET
+      );
+
+      return res.status(200).json({
+        message: "User created",
+        user: { id: user.id, name: user.name, email: user.email, token: token },
+      });
+    } else {
+      return res.status(500).json({ message: error.message });
     }
-
-    const token = jwt.sign(
-      {
-        id: user.id,
-        email: user.email,
-      },
-      process.env.JWT_SECRET
-    );
-
-    return res.status(200).json({
-      message: "User created",
-      user: { id: user.id, name: user.name, email: user.email, token: token },
-    });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: error.message });
+    return res.status(500).json({ message: "Somthing went wrong." });
   }
 };
 
